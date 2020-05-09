20f398d0d402b113acc78266067e9570699865ba
Vianka Rivera <viankalrivera@gmail.com>
2020-05-09 21:29:48 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -51,6 +51,7 @@ def all_supplies_in_holidays(holiday_hash)
     end
       #binding.pry
     end
+  end 
 
   # iterate through holiday_hash and print items such that your readout resembles:
   # Winter:
