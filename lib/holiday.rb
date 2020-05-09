30183e7df9c6baf1b1c9bc3192dce78639025ef0
Vianka Rivera <viankalrivera@gmail.com>
2020-05-09 19:33:29 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -22,7 +22,8 @@ def second_supply_for_fourth_of_july(holiday_supplies)
 end
 
 def add_supply_to_winter_holidays(holiday_hash, supply)
-  holiday_hash[:winter] << "Balloons"
+  holiday_hash[:winter][:christmas] << "Balloons"
+  holiday_hash[:winter][:christmas] << "Balloons"
 
 end
 
