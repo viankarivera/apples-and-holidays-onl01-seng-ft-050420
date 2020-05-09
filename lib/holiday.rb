9215695c933539bcddc2b94d3441d5f489337a01
Vianka Rivera <viankalrivera@gmail.com>
2020-05-09 20:36:06 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -34,7 +34,7 @@ end
 
 def add_new_holiday_with_supplies(holiday_hash, season, holiday_name, supply_array)
   holiday_hash[:fall] << season
-  holiday_hash[:fa]
+  holiday_hash[:fall]
   # remember to return the updated hash
 
 end
