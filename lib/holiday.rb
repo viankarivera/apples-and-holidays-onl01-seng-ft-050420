1e78d28c4a3f0d4ebd8e47cb477e7c74a889017f
Vianka Rivera <viankalrivera@gmail.com>
2020-05-09 19:20:54 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -17,9 +17,7 @@ def second_supply_for_fourth_of_july(holiday_supplies)
        :memorial_day => ["BBQ"]
      }
    }
-   holiday_supplies[:summer][:fourth_of_july][1]
- end
-  second_supply_for_fourth_of_july(holiday_supplies)
+   holiday_supplies[:summer][:fourth_of_july]["1"]
   # return the second element in the 4th of July array
 end
 
