b74d0eaf7814575ca0d9eda05bea51a0ed59b432
Vianka Rivera <viankalrivera@gmail.com>
2020-05-09 21:31:13 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -50,7 +50,7 @@ def all_supplies_in_holidays(holiday_hash)
       arrays = holiday.split("")
     end
       #binding.pry
-    end 
+    end
 
   # iterate through holiday_hash and print items such that your readout resembles:
   # Winter:
