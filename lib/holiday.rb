d20417e9c96281f3c6c0a846842177fbc5bbb6da
Vianka Rivera <viankalrivera@gmail.com>
2020-05-09 20:54:59 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -54,6 +54,7 @@ def all_supplies_in_holidays(holiday_hash)
 end
 
 def all_holidays_with_bbq(holiday_hash)
+  holiday_hash(holidays_with_bbqs)
   # return an array of holiday names (as symbols) where supply lists
   # include the string "BBQ"
 
