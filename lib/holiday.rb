b7534e8c81d978a44bd78c90c4c90b1a14cd8a41
Vianka Rivera <viankalrivera@gmail.com>
2020-05-09 22:21:06 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -71,4 +71,8 @@ end
 def all_holidays_with_bbq(holiday_hash)
   holiday_hash.map do |season, holidays|
     holidays.map do |holiday, supplies|
+      if supplies.include?("BBQ")
+        holiday
+      end
+    end 
 end
