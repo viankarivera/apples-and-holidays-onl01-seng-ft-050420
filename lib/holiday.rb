79995e830d23cca923e1105473778a91b8b0f349
Vianka Rivera <viankalrivera@gmail.com>
2020-05-09 19:10:44 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -17,6 +17,7 @@ def second_supply_for_fourth_of_july(holiday_hash)
        :memorial_day => ["BBQ"]
      }
    }
+   holiday_supplies[:summer][:fourth_of_july][1]
   # return the second element in the 4th of July array
 end
 
