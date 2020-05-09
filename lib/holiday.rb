03ca75c7d3ebf5117208967968bf3d79b94cba66
Vianka Rivera <viankalrivera@gmail.com>
2020-05-09 20:21:39 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -29,7 +29,7 @@ end
 
 
 def add_supply_to_memorial_day(holiday_hash, supply)
-  holiday_hash[:spring][:memorial_day] << "Grill"
+  holiday_hash[:spring][:memorial_day] << supply
   holiday_hash[:spring][:memorial_day] << "Table Cloth"
   holiday_hash[:spring][:memorial_day][0, 1]
 end
