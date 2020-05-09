75733c24f0ca86e9a889ae4dba85fb2796f4fd5c
Vianka Rivera <viankalrivera@gmail.com>
2020-05-09 20:54:42 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -42,6 +42,7 @@ def all_winter_holiday_supplies(holiday_hash)
 end
 
 def all_supplies_in_holidays(holiday_hash)
+  holiday_hash(supplies)
   # iterate through holiday_hash and print items such that your readout resembles:
   # Winter:
   #   Christmas: Lights, Wreath
