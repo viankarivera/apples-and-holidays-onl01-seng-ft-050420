6f9e5153410163e5796361985ba0422423957d0a
Vianka Rivera <viankalrivera@gmail.com>
2020-05-09 22:17:42 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -73,6 +73,6 @@ def all_holidays_with_bbq(holiday_hash)
     holidays.map do |holiday, supplies|
       if supplies.inlude?("BBQ")
       end
-    end
+    end.flatten.compact
       all_holidays_with_bbq(holiday_supplies)
 end
